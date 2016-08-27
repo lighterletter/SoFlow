3f472141c54f50e51102b8d1344dde19839cc222
lighterletter <jgomez818@gmail.com>
2016-08-27 18:37:51 -0400

    created fragment to hold main list

--- SoFlow/app/src/main/java/lighterletter/com/soflow/MainActivity.java
+++ SoFlow/app/src/main/java/lighterletter/com/soflow/MainActivity.java
@@ -1,5 +1,7 @@
 package lighterletter.com.soflow;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initHomeList();
+    }
+
+    public void initHomeList(){
+        FragmentManager fg = getSupportFragmentManager();
+        FragmentTransaction transaction = fg.beginTransaction();
+        HomeFragment homeScreen = new HomeFragment();
+        transaction.replace(R.id.main_container, homeScreen);
+        transaction.commit();
     }
 }
